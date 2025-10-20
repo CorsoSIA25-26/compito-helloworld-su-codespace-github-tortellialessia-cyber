3b664fda1c22f4625e241f8f20ea8e474f5377bb
tortellialessia-cyber <tortelli.alessia@iisleinaudi.edu.it>
2025-10-20 09:19:42 +0200

    Add files via upload

---
+++ areaTriangolo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float baseT, altezzaT, areaT;
+    cout << "Inserire la base del triangolo" << endl;
+      cin >> baseT;
+    cout << "Inserire l'altezza del triangolo" << endl;
+      cin >> altezzaT;
+     areaT=(baseT*altezza)/2;
+    cout << "l'area del triangolo e': " <<areaT <<endl;
+}
